55fa08417fa812c6195259a12a386648b764e80e
susaana <105355603+susaana@users.noreply.github.com>
2022-05-25 17:00:08 +0200

    Create PlaygroundCase.java

---
+++ src/main/CasePackage/PlaygroundCase.java
@@ -0,0 +1,19 @@
+package CasePackage;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+public class PlaygroundCase
+{
+
+    public static void main(String[] arg)
+    {
+        Case case1= new Case("Is it for OP", LocalDateTime.now().plus(3, ChronoUnit.DAYS));
+        Answer a= new Answer("Yes");
+        Answer b= new Answer("NO");
+        case1.getCreatedAt(); 
+
+    }
+}
