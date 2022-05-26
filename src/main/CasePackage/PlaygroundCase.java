3c744f15fe1326f9f34943f0b646d651185d04c2
susaana <105355603+susaana@users.noreply.github.com>
2022-05-26 19:00:57 +0200

    Update PlaygroundCase.java

--- src/main/CasePackage/PlaygroundCase.java
+++ src/main/CasePackage/PlaygroundCase.java
@@ -1,5 +1,6 @@
 package CasePackage;
 import KeywordPackage.Keyword;
+import UserPackage.User;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -11,11 +12,25 @@ public class PlaygroundCase
 
     public static void main(String[] arg)
     {
-        Case case1= new Case("Is it for OP", LocalDateTime.now().plus(3, ChronoUnit.DAYS));
+        Case case1= new Case("Is it for OP", LocalDateTime.now().plus(1,ChronoUnit.DAYS));
         Answer a= new Answer("Yes");
         Answer b= new Answer("NO");
-        case1.getCreatedAt();
-
+        Answer c= new Answer("Egal");
+        System.out.println(case1.getCreatedAt());
+        Voting voting= new Voting("Is it for OP", LocalDateTime.now().plus(1,ChronoUnit.DAYS));
+        voting.setAnswers(a);
+        voting.setAnswers(b);
+        voting.setAnswers(c);
+        System.out.println(voting.getAnswers().size());
+        voting.voting(1);
+        voting.voting(1);
+        voting.voting(2);
+        voting.voting(1);
+        voting.voting(0);
+        System.out.println(voting.getResults());
+        System.out.println(voting.berechneSummeResults());
+        System.out.println(voting.corectAnswer());
+        voting.corectAnswersList();
 
     }
 }
