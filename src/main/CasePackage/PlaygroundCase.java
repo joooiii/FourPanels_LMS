a16ac404b5b781c2fce140a1eca6a544d7c31aa4
susaana <105355603+susaana@users.noreply.github.com>
2022-05-27 21:58:29 +0200

    Update PlaygroundCase.java

--- src/main/CasePackage/PlaygroundCase.java
+++ src/main/CasePackage/PlaygroundCase.java
@@ -13,6 +13,12 @@ public class PlaygroundCase
     public static void main(String[] arg)
     {
         Case case1= new Case("Is it for OP", LocalDateTime.now().plus(1,ChronoUnit.DAYS));
+       case1.setKeywordsProcedure(Keyword.Procedure.BIOTIN_ERGAENZUNG);
+       case1.setKeywordsProcedure(Keyword.Procedure.BURCH_VERFAHREN);
+       case1.setKeywordsSymptoms(Keyword.Symptom.BIOSPSIE_DES_OMENTUMS);
+       case1.setKeywordsSymptoms(Keyword.Symptom.BILIRUBIN_IM_BLUT_DIREKT_ERNIEDRIGT);
+        System.out.println(case1.getKeywordsProcedure());
+        System.out.println(case1.getKeywordsSymptoms());
         Answer a= new Answer("Yes");
         Answer b= new Answer("NO");
         Answer c= new Answer("Egal");
@@ -28,9 +34,8 @@ public class PlaygroundCase
         voting.voting(1);
         voting.voting(0);
         System.out.println(voting.getResults());
-        System.out.println(voting.berechneSummeResults());
-        System.out.println(voting.corectAnswer());
+        System.out.println("Votet " + voting.berechneSummeResults() + " People");
+        System.out.println("Maximum votes have answer: " + voting.corectAnswer());
         voting.corectAnswersList();
-
     }
 }
