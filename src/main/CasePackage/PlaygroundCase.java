a885e51a054d540095134eb7f71834b35ce7bdac
susaana <105355603+susaana@users.noreply.github.com>
2022-05-31 20:07:48 +0200

    Update PlaygroundCase.java

--- src/main/CasePackage/PlaygroundCase.java
+++ src/main/CasePackage/PlaygroundCase.java
@@ -7,6 +7,8 @@ import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 
+import static validation.Ensurer.checkState;
+
 public class PlaygroundCase
 {
 
@@ -20,6 +22,7 @@ public class PlaygroundCase
        case1.setKeywordsSymptoms(Keyword.Symptom.BILIRUBIN_IM_BLUT_DIREKT_ERNIEDRIGT);
        case1.setKeywordProfesions(Keyword.Profession.ANAESTHESIOLOGIE);
        case1.setKeywordProfesions(Keyword.Profession.CHIRUGIE);
+       // case1.addMember();
 
         Voting voting= new Voting("Is it for OP?", LocalDateTime.now().plus(1,ChronoUnit.DAYS));
 
@@ -32,18 +35,22 @@ public class PlaygroundCase
         Answer a= new Answer("Yes");
         Answer b= new Answer("NO");
         Answer c= new Answer("Egal");
+        // Muss mindest 2 Antowrt sein
+        checkState(voting.getAnswers().size() < 2, "Es muss mindest 2 Antwort sein");
         voting.setAnswers(a);
         voting.setAnswers(b);
         voting.setAnswers(c);
+
         voting.voting(1,1, case1);
-        voting.voting(1,2, case1);
+        voting.voting(3,2, case1);
         voting.voting(2,3, case1);
-        voting.voting(1,1, case1);
+        voting.voting(4,1, case1);
         voting.voting(0, 1, case1);
         System.out.println(voting.getResults());
         System.out.println("Votet " + voting.berechneSummeResults() + " People");
         System.out.println("Maximum votes have answer: " + voting.corectAnswer());
         voting.corectAnswersList();
+        voting.addScoreForRightAnswer();
 
     }
 }
