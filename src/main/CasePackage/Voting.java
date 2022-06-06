50dee990e000924f773544c5497dfaec3fdc91be
susaana <105355603+susaana@users.noreply.github.com>
2022-06-06 22:45:51 +0200

    Update Voting.java

--- src/main/CasePackage/Voting.java
+++ src/main/CasePackage/Voting.java
@@ -92,6 +92,7 @@ public class Voting
             }
             if (a == null)
             {
+
                 System.out.println("Add new Answer");
                 Scanner input = new Scanner(System.in);
                 String newAnswer = input.nextLine();
